c36c880bd2f1c2984a2797475fd522672da3f0ea
Rakad Kh <rakad.khawaled@gmail.com>
2021-01-28 16:50:07 +0200

    select btn done

---
+++ src/components/buttons/SelectButton/SelectButton.js
@@ -0,0 +1,50 @@
+import './SelectButton.css';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 90,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+export default function SelectButton({ setData, header, content, data }) {
+  const classes = useStyles();
+  const handleChange = (event) => {
+    setData(event.target.value);
+    // console.log(data)
+  };
+
+  return (
+    <div>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-label">{header}</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={data}
+          defaultValue=""
+          onChange={handleChange}
+          className={classes.selectEmpty}
+        >
+          {(() => {
+            const options = [];
+            for (let elem of content) {
+              options.push(
+                <MenuItem key={elem} value={elem}>
+                  {elem}
+                </MenuItem>
+              );
+            }
+            return options;
+          })()}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
