3d6f6f20acfd38595397233c6a30de8ca4481413
Rakad Kh <rakad.khawaled@gmail.com>
2021-01-28 17:00:55 +0200

    navbar done '

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import NavBarButton from '../NavBarButton';
+import Logo from '../Logo';
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <div className="navBar">
+      <NavBarButton text="הוספת תורים" path="/" />
+      <NavBarButton text="תורים מתוכננים" path="/" />
+      <NavBarButton text="חיפוש משתמשים" path="/" />
+      <Logo />
+    </div>
+  );
+};
+
+export default NavBar;
