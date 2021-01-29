e791516d6c88170ced38398d739b56189d09539f
Rakad Kh <rakad.khawaled@gmail.com>
2021-01-29 17:15:29 +0200

    editing Dashboard.js

--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -1,5 +1,11 @@
-import NavBar from '../../NavBar';
-
-const Dashboard = () => <NavBar />;
+import AppointmentsContainer from '../../AppointmentsContainer';
+import AppointmentForm from '../../AppointmentForm';
+import { Fragment } from 'react';
+const Dashboard = () => (
+    <Fragment>
+        <AppointmentForm />
+        <AppointmentsContainer />
+    </Fragment>
+);
 
 export default Dashboard;
