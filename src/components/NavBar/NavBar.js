ab9d2a313b5d2c90f74b4a3b7eb39c22ff370d6b
Rakad Kh <rakad.khawaled@gmail.com>
2021-01-29 17:51:24 +0200

    Logo component was added by Nizar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,18 +1,14 @@
-import { NavBarButton } from '../buttons';
+import NavBarButton from '../NavBarButton';
 import Logo from '../Logo';
 import './NavBar.css';
 
 const NavBar = () => {
   return (
     <div className="navBar">
-      <div className="buttons">
-        <NavBarButton text="חיפוש משתמשים" path="/usersSearch" />
-        <NavBarButton text="תורים מתוכננים" path="/bookedAppointments" />
-        <NavBarButton text="הוספת תורים" path="/" />
-      </div>
-      <div className="logo">
-        <Logo />
-      </div>
+      <NavBarButton text="הוספת תורים" path="/" />
+      <NavBarButton text="תורים מתוכננים" path="/" />
+      <NavBarButton text="חיפוש משתמשים" path="/" />
+      <Logo />
     </div>
   );
 };
