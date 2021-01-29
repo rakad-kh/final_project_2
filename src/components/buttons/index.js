cb810254ae4c3fa15362700aa0186a873ab22bb3
Nizar Halloun <nshalloun@hotmail.com>
2021-01-29 20:37:41 +0200

    index

--- src/components/buttons/index.js
+++ src/components/buttons/index.js
@@ -2,5 +2,12 @@ import NavBarButton from './NavBarButton';
 import ConfirmButton from './ConfirmButton';
 import SelectButton from './SelectButton';
 import DateTimeButton from './DateTimeButton';
+import DeleteAppointmentButton from './DeleteAppointmentButton';
 
-export { ConfirmButton, SelectButton, DateTimeButton, NavBarButton };
+export {
+  ConfirmButton,
+  SelectButton,
+  DateTimeButton,
+  NavBarButton,
+  DeleteAppointmentButton,
+};
