3410937e379323d236665f4d64f20c1bac3cbac4
Rakad Kh <rakad.khawaled@gmail.com>
2021-01-30 12:07:19 +0200

    d

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,18 @@
-import { NavBarButton } from '../buttons'
+import { NavBarButton } from '../buttons';
 import Logo from '../Logo';
 import './NavBar.css';
 
 const NavBar = () => {
   return (
     <div className="navBar">
-      <NavBarButton text="הוספת תורים" path="/" />
-      <NavBarButton text="תורים מתוכננים" path="/" />
-      <NavBarButton text="חיפוש משתמשים" path="/" />
-      <Logo />
+      <div className="buttons">
+        <NavBarButton text="חיפוש משתמשים" path="/usersSearch" />
+        <NavBarButton text="תורים מתוכננים" path="/bookedAppointments" />
+        <NavBarButton text="הוספת תורים" path="/" />
+      </div>
+      <div className="logo">
+        <Logo />
+      </div>
     </div>
   );
 };
