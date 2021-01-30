95283820fec1a4abdba0074c1aa414b924857c5c
Nizar Halloun <nshalloun@hotmail.com>
2021-01-30 15:01:57 +0200

    icon

--- src/components/pages/BookedAppointments/BookedAppointments.js
+++ src/components/pages/BookedAppointments/BookedAppointments.js
@@ -2,6 +2,8 @@ import { SelectButton } from '../../buttons';
 import { useState } from 'react';
 import BookedAppointmentsContainer from '../../BookedAppointmentsContainer';
 
+import AddLocationOutlinedIcon from '@material-ui/icons/AddLocationOutlined';
+
 import './BookedAppointments.css';
 const hospitalName = [
   'איכילוב',
@@ -16,6 +18,7 @@ const BookedAppointments = () => {
   return (
     <div>
       <div className="selectButtonContainer">
+        <AddLocationOutlinedIcon style={{ marginTop: 29 }} />
         <SelectButton
           header={'בית חולים'}
           content={hospitalName}
