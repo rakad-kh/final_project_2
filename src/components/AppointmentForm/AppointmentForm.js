7ff9b92901ff9ac541a1038853a832f6a57b1517
Nizar Halloun <nshalloun@hotmail.com>
2021-02-04 10:40:16 +0200

    logic

--- src/components/AppointmentForm/AppointmentForm.js
+++ src/components/AppointmentForm/AppointmentForm.js
@@ -12,15 +12,34 @@ const hospitalName = [
   'סורוקה',
 ];
 const squaresNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const bloodKind = ["לא ידועה", "+A", "-A", "+B", "-B", "+AB"];
-const msgContent = ["הודעת ברירת מחדל", "zichronmenachem@gmail.com הודעת שגיאה "];
+const bloodKindArray = ['לא ידועה', '+A', '-A', '+B', '-B', '+AB'];
+const msgContentArray = [
+  'הודעת ברירת מחדל',
+  'zichronmenachem@gmail.com הודעת שגיאה ',
+];
 
-const AppointmentForm = () => {
-  const [donation, setDonation] = useState('טרומבוציטים');
+const AppointmentForm = ({ addAppToArray }) => {
+  const [donation, setDonation] = useState('');
   const [hospital, setHospital] = useState('');
   const [date, setDate] = useState('');
   const [hour, setHour] = useState('');
   const [squares, setSquares] = useState('');
+
+  const [msgContent, setMsgContent] = useState('');
+  const [bloodKind, setBloodKind] = useState('');
+
+  const isButtonDisable = () =>
+    !(donation && hospital && date && hour && squares);
+
+  const onClick = () => {
+    addAppToArray({ donation, hospital, date, hour, squares });
+
+    setDonation('');
+    setHospital('');
+    setDate('');
+    setHour('');
+    setSquares('');
+  };
 
   return (
     <div className="appointmentForm">
@@ -46,17 +65,29 @@ const AppointmentForm = () => {
       <SelectButton header={'משבצות'} content={squaresNum} setData={setSquares}>
         data={squares}
       </SelectButton>
-      {donation === "גרנולוציטים" ?
-        <SelectButton header={'תוכן הודעה'} content={msgContent} setData={setSquares}>
-          {' '}
+      {donation === 'גרנולוציטים' ? (
+        <SelectButton
+          header={'תוכן הודעה'}
+          content={msgContentArray}
+          setData={setMsgContent}
+          data={msgContent}
+        >
           data={squares}
-        </SelectButton> : null}
-      {donation === "גרנולוציטים" ?
-        <SelectButton header={'סוג דם'} content={bloodKind} setData={setSquares}>
-          {' '}
-          data={squares}
-        </SelectButton> : null}
-      <ConfirmButton text="הוספה" />
+        </SelectButton>
+      ) : null}
+      {donation === 'גרנולוציטים' ? (
+        <SelectButton
+          header={'סוג דם'}
+          content={bloodKindArray}
+          setData={setBloodKind}
+          data={bloodKind}
+        ></SelectButton>
+      ) : null}
+      <ConfirmButton
+        text="הוספה"
+        disabled={isButtonDisable()}
+        onClick={onClick}
+      />
     </div>
   );
 };
--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -1,11 +1,19 @@
 import AppointmentsContainer from '../../AppointmentsContainer';
 import AppointmentForm from '../../AppointmentForm';
-import { Fragment } from 'react';
-const Dashboard = () => (
+import { Fragment, useState } from 'react';
+const Dashboard = () => {
+  const [appArray, setAppArray] = useState([]);
+  const addAppToArray = (app) => {
+    appArray.push(app);
+    setAppArray([...appArray]);
+  };
+
+  return (
     <Fragment>
-        <AppointmentForm />
-        <AppointmentsContainer />
+      <AppointmentForm addAppToArray={addAppToArray} />
+      <AppointmentsContainer appArray={appArray} />
     </Fragment>
-);
+  );
+};
 
 export default Dashboard;
